d5af8e49e8fae3d64b56a490c144afc8f96210de
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-01-17 16:22:15 -0500

    exercise6 commit

---
+++ exercise6.rb
@@ -0,0 +1,22 @@
+grocery_list = ["milk", "eggs","pasta", "bacon", "salmon"]
+
+def item(grocery_list)
+  grocery_list.each
+    puts "* " + item
+  end
+end
+
+
+  grocery_list << "rice"
+
+  grocery_list.length
+
+  grocery_list.include?("banana")
+
+  grocery_list[1]
+
+  grocery_list.sort
+
+  grocery_list.pop("salmon")
+
+  grocery_list
